97e2953dc4e77efb770568fb8f6cfcf994a4fd35
Ryan Walker <ryanjwalker2001@gmail.com>
2024-07-17 19:55:04 +0100

    add

--- config.nix
+++ config.nix
@@ -1,12 +1,19 @@
-{ ... } @ inputs: {
+{ ... } @ inputs:
+let
+  # Don't change without folling thse steps https://nix-community.github.io/NixOS-WSL/how-to/change-username.html
+  user = "ryan";
+in
+{
   imports = [
     ./modules/wsl-vpn.nix
   ];
 
   system.stateVersion = "24.05";
-  wsl.enable = true;
 
-  wsl-vpn.enable = true;
+  wsl = {
+    enable = true;
+    defaultUser = user;
+  };
 
   nix.settings = {
     trusted-users = [ "root" "@wheel" ];
@@ -17,7 +24,9 @@
   i18n.defaultLocale = "en_GB.UTF-8";
 
   home-manager = {
-    extraSpecialArgs = { inherit inputs; };
+    extraSpecialArgs = { inherit inputs user; };
     users.nixos = import ./home.nix;
   };
+
+  wsl-vpn.enable = true;
 }
---
+++ home.nix
@@ -0,0 +1,7 @@
+{ user, ... }: {
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = "24.05";
+  };
+}
