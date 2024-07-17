b54e2c2e92ac7f45dfc1615b0ac8aec14363eabe
Ryan Walker <ryanjwalker2001@gmail.com>
2024-07-17 17:54:25 +0100

    trusted users

--- config.nix
+++ config.nix
@@ -3,7 +3,10 @@
 
   wsl-vpn.enable = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    trusted-users = [ "root" "@wheel" ];
+    experimental-features = [ "nix-command" "flakes" ];
+  };
 
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
