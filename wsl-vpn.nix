c9c45e114db8e8381f0c461bbe10808d50a83a34
Ryan Walker <ryanjwalker2001@gmail.com>
2024-07-17 17:37:18 +0100

    fix config

--- wsl-vpn.nix
+++ wsl-vpn.nix
@@ -11,58 +11,60 @@ in
     };
   };
 
-  systemd.services = {
-    wsl-vpnkit-auto = {
-      enable = cfg.enable;
-      description = "wsl-vpnkit";
+  config = lib.mkIf cfg.enable {
+    systemd.services = {
+      wsl-vpnkit-auto = {
+        enable = true;
+        description = "wsl-vpnkit";
 
-      path = [pkgs.iputils];
-      script = ''
-        has_internet () {
-          ping -q -w 1 -c 1 8.8.8.8 >/dev/null
-        }
+        path = [ pkgs.iputils ];
+        script = ''
+          has_internet () {
+            ping -q -w 1 -c 1 8.8.8.8 >/dev/null
+          }
 
-        has_company_network () {
-          ping -q -w 1 -c 1 ${cfg.checkURL} >/dev/null
-        }
+          has_company_network () {
+            ping -q -w 1 -c 1 ${cfg.checkURL} >/dev/null
+          }
 
-        is_active_wsl-vpnkit () {
-          systemctl is-active -q wsl-vpnkit.service
-        }
+          is_active_wsl-vpnkit () {
+            systemctl is-active -q wsl-vpnkit.service
+          }
 
-        main () {
-          if is_active_wsl-vpnkit; then
-            if has_internet && ! has_company_network; then
-              echo "Stopping wsl-vpnkit..."
-              systemctl stop wsl-vpnkit.service
+          main () {
+            if is_active_wsl-vpnkit; then
+              if has_internet && ! has_company_network; then
+                echo "Stopping wsl-vpnkit..."
+                systemctl stop wsl-vpnkit.service
+              fi
+            else
+              if ! has_internet; then
+                echo "Starting wsl-vpnkit..."
+                systemctl start wsl-vpnkit.service
+              fi
             fi
-          else
-            if ! has_internet; then
-              echo "Starting wsl-vpnkit..."
-              systemctl start wsl-vpnkit.service
-            fi
-          fi
-        }
+          }
 
-        while :
-        do
-          main
-          sleep 5
-        done
-      '';
+          while :
+          do
+            main
+            sleep 5
+          done
+        '';
 
-      wantedBy = ["multi-user.target"];
-    };
+        wantedBy = [ "multi-user.target" ];
+      };
 
-    wsl-vpnkit = {
-      enable = cfg.enable;
-      description = "wsl-vpnkit";
+      wsl-vpnkit = {
+        enable = true;
+        description = "wsl-vpnkit";
 
-      serviceConfig = {
-        ExecStart = "${pkgs.wsl-vpnkit}/bin/wsl-vpnkit";
-        Type = "idle";
-        Restart = "always";
-        KillMode = "mixed";
+        serviceConfig = {
+          ExecStart = "${pkgs.wsl-vpnkit}/bin/wsl-vpnkit";
+          Type = "idle";
+          Restart = "always";
+          KillMode = "mixed";
+        };
       };
     };
   };
