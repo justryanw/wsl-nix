a0e8cf993988ff41584b3f1ed7b6f35f5b23ff02
Ryan Walker <ryanjwalker2001@gmail.com>
2024-07-17 20:40:36 +0100

    home manager programs

--- home.nix
+++ home.nix
@@ -4,4 +4,24 @@
     homeDirectory = "/home/${user}";
     stateVersion = "24.05";
   };
+
+  programs = {
+    zsh = {
+      enable = true;
+      history.size = 1000;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases.la = "ls -A";
+    };
+
+    starship.enable = true;
+
+    git = {
+      enable = true;
+      userName = "Ryan Walker";
+      userEmail = "ryanjwalker2001@gmail.com";
+      extraConfig.pull.rebase = false;
+      aliases.acm = "!git add -A && git commit -m";
+    };
+  };
 }
